aad654d8a706719df363ef4ab82516039ec9de1b
bic-pannawat <bic-pannawat@biccorp.local>
2025-01-07 17:20:16 +0700

    TESTPHP

---
+++ php/src/basic.php
@@ -0,0 +1,47 @@
+<?php
+    // $name = "ARM";
+    // $food = 'pizza';
+    // $email = "fake123@gmail.com";
+
+    // $age = 23;
+    // $users = 2;
+    // $quantity = 3;
+
+    // $gpa = 2.5;
+    // $price = 4.99;
+    // $tax_rate = 5.1;
+
+    // $employed = true;
+    // $online = false;
+    // $for_sale = true;
+
+    // echo "Hello {$name} <br>";
+    // echo "You like {$food} <br>";
+    // echo "Your email is {$email} <br>";
+
+    // echo "You are {$age} years old <br>";
+    // echo "There are {$users} users online <br>";
+    // echo "You would like to buy {$quantity} items <br>";
+
+    // echo "Your GPA is: {$gpa} <br>";
+    // echo "Your pizza is {$price} <br>";
+    // echo "The sales tax rate is: {$tax_rate} <br>";
+
+    // //เขียนได้สองแบบ
+    // $total = $quantity * $price;
+    // echo "Your total is: {$total} <br>";
+    // echo "Your total is: " . ($quantity * $price) . "<br>";
+
+    //Arithmetic operators
+    // + - * / ** %
+    // $x = 10;
+    // $y = 2;
+    // $z = null;
+
+    // $z = $x % $y;
+    // echo $z;
+
+    // $counter = 10;
+    // $counter += 10;
+    // echo $counter;
+?>
--- php/src/index.php
+++ php/src/index.php
@@ -1,35 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <label>username: </label><br>
+    <input type="text" name="username"><br>
+    <input type="password" name="password"><br>
+    <input type="submit" value="login">
+</body>
+</html>
+
 <?php
-    $name = "ARM";
-    $food = 'pizza';
-    $email = "fake123@gmail.com";
-
-    $age = 23;
-    $users = 2;
-    $quantity = 3;
-
-    $gpa = 2.5;
-    $price = 4.99;
-    $tax_rate = 5.1;
-
-    $employed = true;
-    $online = false;
-    $for_sale = true;
-
-    echo "Hello {$name} <br>";
-    echo "You like {$food} <br>";
-    echo "Your email is {$email} <br>";
-
-    echo "You are {$age} years old <br>";
-    echo "There are {$users} users online <br>";
-    echo "You would like to buy {$quantity} items <br>";
-
-    echo "Your GPA is: {$gpa} <br>";
-    echo "Your pizza is {$price} <br>";
-    echo "The sales tax rate is: {$tax_rate} <br>";
-
-    //เขียนได้สองแบบ
-    $total = $quantity * $price;
-    echo "Your total is: {$total} <br>";
-    echo "Your total is: " . ($quantity * $price) . "<br>";
-
+    //$_GET, $_POST
 ?>
